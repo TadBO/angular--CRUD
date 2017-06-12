e1ffb0b3857a88568811ce976f8cf52176011937
TadBO <986520756@qq.com>
2017-06-12 19:54:36 +0800

    '项目初始化完成'

---
+++ todomvc-app-template-master/js/app.js
@@ -0,0 +1,10 @@
+(function (window, angular) {
+	//'use strict';
+
+	// Your starting point. Enjoy the ride!
+	var app = angular.module('todoApp', []);
+	app.controller('todoCtrl', function ($scope) {
+
+    });
+
+})(window, angular);
