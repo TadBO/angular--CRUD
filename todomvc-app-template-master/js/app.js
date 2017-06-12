4367b301486601c6c510d82d4d909b7f5282c4a0
TadBO <986520756@qq.com>
2017-06-12 20:00:06 +0800

    '完成查看数据'

--- todomvc-app-template-master/js/app.js
+++ todomvc-app-template-master/js/app.js
@@ -4,6 +4,14 @@
 	// Your starting point. Enjoy the ride!
 	var app = angular.module('todoApp', []);
 	app.controller('todoCtrl', function ($scope) {
+		//确定数据为数组类型
+		$scope.todoList = [
+			{text: 'css', status: false},
+			{text: 'html', status: false},
+			{text: 'javascript', status: false},
+			{text: 'angular', status: true}
+		];
+		//查看数据
 
     });
 
